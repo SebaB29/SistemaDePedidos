6f3a410380fb718808aa8a01a341007fc4e83b1e
MatiasRueda <matiasdruedaa@gmail.com>
2024-10-25 06:26:54 -0300

    refactorizo codigo y agrego ResponseEntity al "/product/create"

--- src/main/java/com/ing_software_grupo8/sistema_de_pedidos/controller/ProductController.java
+++ src/main/java/com/ing_software_grupo8/sistema_de_pedidos/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.ing_software_grupo8.sistema_de_pedidos.controller;
 
 import com.ing_software_grupo8.sistema_de_pedidos.DTO.AdminCreateProductRequestDTO;
+import com.ing_software_grupo8.sistema_de_pedidos.DTO.MessageResponseDTO;
 import com.ing_software_grupo8.sistema_de_pedidos.entity.Product;
 import com.ing_software_grupo8.sistema_de_pedidos.entity.Stock;
 import com.ing_software_grupo8.sistema_de_pedidos.entity.Attribute;
@@ -10,6 +11,7 @@ import com.ing_software_grupo8.sistema_de_pedidos.service.IProductService;
 import com.ing_software_grupo8.sistema_de_pedidos.service.IStockService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,29 +29,47 @@ public class ProductController {
     @Autowired
     private IAttributeService attributeService;
 
+    private long createProduct(CustomUserDetails user, AdminCreateProductRequestDTO productRequest) {
+        Product product = new Product();
+        product.setName(productRequest.getProductName());
+        product.setWeight(productRequest.getWeight());
+        return productService.createProduct(user, product);
+    }
+
+    private void createStock(long productId, CustomUserDetails user, AdminCreateProductRequestDTO productRequest) {
+        Stock stock = new Stock();
+        stock.setProductId(productId);
+        stock.setStockType(productRequest.getStockType());
+        stock.setQuantity(productRequest.getQuantity());
+        stockService.createStock(user, stock);
+    }
+
+    private void createAttribute(long productId, CustomUserDetails user, AdminCreateProductRequestDTO productRequest) {
+        Attribute attribute = new Attribute();
+        attribute.setProductId(productId);
+        attribute.setDescription(productRequest.getDescription());
+        attributeService.createAttribute(user, attribute);
+    }
+
+    public void createProductWithStockAndAttributes(CustomUserDetails user,
+            AdminCreateProductRequestDTO productRequest) {
+        long productId = createProduct(user, productRequest);
+        createStock(productId, user, productRequest);
+        createAttribute(productId, user, productRequest);
+    }
+
     @PostMapping("/product/create")
-    public String create(@AuthenticationPrincipal CustomUserDetails user,
+    public ResponseEntity<MessageResponseDTO> create(@AuthenticationPrincipal CustomUserDetails user,
             @RequestBody AdminCreateProductRequestDTO productRequest) {
         try {
-            Product product = new Product();
-            product.setName(productRequest.getProductName());
-            product.setWeight(productRequest.getWeight());
-            long productId = productService.createProduct(user, product);
-
-            Stock stock = new Stock();
-            stock.setProductId(productId);
-            stock.setStockType(productRequest.getStockType());
-            stock.setQuantity(productRequest.getQuantity());
-            stockService.createStock(user, stock);
-
-            Attribute attribute = new Attribute();
-            attribute.setProductId(productId);
-            attribute.setDescription(productRequest.getDescription());
-            attributeService.createAttribute(user, attribute);
-
-            return String.format("Product %s created!", productRequest.getProductName());
+            createProductWithStockAndAttributes(user, productRequest);
+            String messageOk = "Product created";
+            MessageResponseDTO message = new MessageResponseDTO(messageOk);
+            return ResponseEntity.ok(message);
         } catch (IllegalArgumentException e) {
-            return String.format("Error!");
+            String messageError = "Unexpected Error";
+            MessageResponseDTO message = new MessageResponseDTO(messageError);
+            return ResponseEntity.badRequest().body(message);
         }
     }
 }
