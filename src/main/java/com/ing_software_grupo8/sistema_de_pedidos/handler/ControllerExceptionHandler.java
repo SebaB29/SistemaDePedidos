e872bf4a3a2e537e1c0b1ee4ac50facfefa54ebc
agustin <agustinvallcorba1698@gmail.com>
2024-10-31 01:54:49 -0300

    Adapto Exception handler al nuevo Response

--- src/main/java/com/ing_software_grupo8/sistema_de_pedidos/handler/ControllerExceptionHandler.java
+++ src/main/java/com/ing_software_grupo8/sistema_de_pedidos/handler/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.ing_software_grupo8.sistema_de_pedidos.handler;
 
 import com.ing_software_grupo8.sistema_de_pedidos.exception.ApiException;
+import com.ing_software_grupo8.sistema_de_pedidos.response.GenericResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,17 +12,29 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<String> handleException(NoHandlerFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<GenericResponse> handleException(NoHandlerFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body(GenericResponse.builder()
+                                                  .message(e.getMessage())
+                                                  .status(HttpStatus.NOT_FOUND)
+                                                  .build());
     }
 
     @ExceptionHandler(ApiException.class)
-    public ResponseEntity<ApiException> handleApiException(ApiException e) {
-        return ResponseEntity.status(e.getStatusCode()).body(e);
+    public ResponseEntity<GenericResponse> handleApiException(ApiException e) {
+        return ResponseEntity.status(e.getStatusCode())
+                             .body(GenericResponse.builder()
+                                                    .message(e.getMessage())
+                                                    .status(e.getCode())
+                                                    .build());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+    public ResponseEntity<GenericResponse> handleException() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                             .body(GenericResponse.builder()
+                                                  .message("Internal server error")
+                                                  .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                                                  .build());
     }
 }
