4b59840d53017f6d2613b83e1aefdadc24ee52c7
SebaB29 <sebita29@gmail.com>
2024-11-01 16:50:45 -0300

    Soluciono problemas de compilacion

--- src/main/java/com/ing_software_grupo8/sistema_de_pedidos/entity/User.java
+++ src/main/java/com/ing_software_grupo8/sistema_de_pedidos/entity/User.java
@@ -2,12 +2,14 @@ package com.ing_software_grupo8.sistema_de_pedidos.entity;
 
 import com.ing_software_grupo8.sistema_de_pedidos.DTO.UserRequestDTO;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
