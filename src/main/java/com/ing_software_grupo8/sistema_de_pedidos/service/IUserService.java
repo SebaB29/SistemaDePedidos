fb895440fb9bea77215dc2a1959823d78d44cb15
SebaB29 <sebita29@gmail.com>
2024-12-03 21:06:42 -0300

    Refactor UserService

--- src/main/java/com/ing_software_grupo8/sistema_de_pedidos/service/IUserService.java
+++ src/main/java/com/ing_software_grupo8/sistema_de_pedidos/service/IUserService.java
@@ -1,17 +1,15 @@
 package com.ing_software_grupo8.sistema_de_pedidos.service;
 
-import java.util.Optional;
-
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.ing_software_grupo8.sistema_de_pedidos.DTO.MessageResponseDTO;
 import com.ing_software_grupo8.sistema_de_pedidos.DTO.UserRequestDTO;
 import com.ing_software_grupo8.sistema_de_pedidos.entity.User;
-
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.Optional;
 
 public interface IUserService {
 
-    MessageResponseDTO editUser(UserRequestDTO userRequestDTO, HttpServletRequest request) throws JsonMappingException;
+    MessageResponseDTO editUser(UserRequestDTO userRequestDTO, HttpServletRequest request);
 
-    Optional<User> getUser(String userEmail);;
+    Optional<User> getUser(String userEmail);
 }
